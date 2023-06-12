7a3ae6a3990122ddfc036f3017c9ba1a183b612f
unknown <juancastelblancoh@gmail.com>
2023-06-12 14:09:44 -0500

    Vercel config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import * as cron from 'node-cron';
-import { Service } from './utils/service';
+import { Service } from '../utils/service';
 
 const service = new Service();
 
@@ -15,11 +15,17 @@ const PORT_NUMBER = 80;
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send('Alive');
 });
 
 app.get('/last', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send(service.getData());
 });
 
 app.listen(PORT_NUMBER, () => console.log('Server running on por', PORT_NUMBER,new Date()));
+
+module.exports = app;
