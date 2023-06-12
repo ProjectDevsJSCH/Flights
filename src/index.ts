9cf4ebdb18373618471c29e1d5910be2cf66618a
unknown <juancastelblancoh@gmail.com>
2023-06-12 13:44:36 -0500

    Changed port

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,11 @@ cron.schedule('*/30 * * * * *', async () => {
   await service.setDataResponse();
 });
 
-const PORT_NUMBER = 5000;
+const PORT_NUMBER = 80;
 const app = express();
 
 app.get('/last', (req: Request, res: Response) => {
   res.send(service.getData());
 });
 
-app.listen(PORT_NUMBER, () => console.log('Server running on port 5000', new Date()));
+app.listen(PORT_NUMBER, () => console.log('Server running on por', PORT_NUMBER,new Date()));
